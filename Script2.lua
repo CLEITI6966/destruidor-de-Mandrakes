32899b56e031380491aa49bef0adb4ed09d6cf72
CLEITI6966 <biungreu@gmail.com>
2025-02-15 16:46:03 -0300

    Update Script2.lua

--- Script2.lua
+++ Script2.lua
@@ -1,3 +1,17 @@
+while true do
+    channel:SendAsync("nunca iria largar aquela sua buceta gostosa")
+    channel:SendAsync("gostoso bora gozar")
+    channel:SendAsync(":hot_face:")
+    channel:SendAsync("ainpara")
+    channel:SendAsync("vai devagar")
+    channel:SendAsync("me come logo")
+    channel:SendAsync("tu deu o cu ontem com aquela menina né")
+    channel:SendAsync("seu vagabundo seu inútil seu filho da puta tomara que você morra")
+    channel:SendAsync("desgraçado do carai")
+    
+    wait(6.7)
+end"
+
 wait(5)
 
 local soundId = "rbxassetid://9043346124"
